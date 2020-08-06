6dfacaf2f4c92f94178e22c5bc82148ef6dc587e
John Harris <joharris@vmware.com>
2020-08-06 08:20:32 -0700

    GoT
    Signed-off-by: John Harris <joharris@vmware.com>

--- bootstrap/slapd-init.sh
+++ bootstrap/slapd-init.sh
@@ -4,10 +4,10 @@ set -eu
 readonly DATA_DIR="/bootstrap/data"
 readonly CONFIG_DIR="/bootstrap/config"
 
-readonly LDAP_DOMAIN=planetexpress.com
-readonly LDAP_ORGANISATION="Planet Express, Inc."
-readonly LDAP_BINDDN="cn=admin,dc=planetexpress,dc=com"
-readonly LDAP_SECRET=GoodNewsEveryone
+readonly LDAP_DOMAIN=gameofthrones.com
+readonly LDAP_ORGANISATION="Game of Thrones"
+readonly LDAP_BINDDN="cn=admin,dc=gameofthrones,dc=com"
+readonly LDAP_SECRET=winteriscoming
 
 readonly LDAP_SSL_KEY="/etc/ldap/ssl/ldap.key"
 readonly LDAP_SSL_CERT="/etc/ldap/ssl/ldap.crt"
